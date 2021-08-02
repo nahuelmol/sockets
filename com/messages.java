a360ece41a7b50e63c266e0e55c3770300444adb
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-01 21:47:09 -0300

    Update messages.java

--- com/messages.java
+++ com/messages.java
@@ -1,5 +1,33 @@
 package com;
 
+interface Message {
+    void Content(String c);
+    void Destiny(String d);
+    void Sender(String s);
+}
+
+class AlertMessage implements Message {
+
+    String Cont;
+    String Dest;
+    String Send;
+
+    @Override
+    public void Content(String newContent){
+        Cont = newContent
+    };
+
+    @Override
+    public void Destiny(String newDestiny){
+        Dest = newDestiny;
+    };
+
+    @Sender
+    public voud Sender(String newSender){
+        Send = newSender;
+    };
+}
+
 public class messages {
 	public static void FirstContact(){
 		System.out.println("Hello");
